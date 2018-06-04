b65fd61bacce165e19e59e04a18fbace56688420
Justin Moen <jamoen7@gmail.com>
2018-06-03 21:41:37 -0500

    Delete old files

--- test_utils.py
+++
@@ -1,8 +0,0 @@
-import utils
-
-
-def test_serialization():
-    input = {}
-    serialized = utils.serialize(input)
-    deserialized = utils.deserialize(serialized)
-    assert deserialized == input

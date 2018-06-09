4615ce846e14dd7ec5aa862790a1e9c902184f86
Justin Moen <jamoen7@gmail.com>
2018-06-09 12:40:42 -0500

    Add "peer list" widget to GUI

--- gui.py
+++ gui.py
@@ -8,13 +8,11 @@ root = tk.Tk()
 class App(tk.Frame):
     def __init__(self, port, host, master=None, add_local=False):
         super().__init__(master)
-
         self.peer = FileSharingPeer(8, port, host)
         if add_local:
             self.peer.addlocalfile('hello.txt')
 
-        self.thread = threading.Thread(
-            target=self.peer.main_loop, args=([]))
+        self.thread = threading.Thread(target=self.peer.main_loop, args=([]))
         self.thread.start()
 
         self.pack()
@@ -26,17 +24,22 @@ class App(tk.Frame):
         self.after(3000, self.on_timer)
 
     def on_refresh(self):
-        """copy state from `self.state.some_va r` over to `self.some_var"""
-        self.num_peers.set(self.peer.num_peers())
+        self.refresh_peer_list()
+
+    def refresh_peer_list(self):
+        # Delete previous, stale peers
+        if self.peer_list.size() > 0:
+            self.peer_list.delete(0, self.peer_list.size() - 1)
+        # Copy over current peers
+        for remote_peer in self.peer.remote_peers:
+            self.peer_list.insert(tk.END, remote_peer.id)
 
     def create_widgets(self):
-        self.num_peers = tk.IntVar()
-        self.num_peers.text = self.peer.num_peers
-        self.num_peers_label = tk.Label(root, textvariable=self.num_peers)
-        self.num_peers_label.pack(side='top')
+        self.peer_list = tk.Listbox()
+        self.peer_list.pack()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
+        self.quit = tk.Button(
+            self, text="QUIT", fg="red", command=root.destroy)
         self.quit.pack(side="bottom")
 
 
